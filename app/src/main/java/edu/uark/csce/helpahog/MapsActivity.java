29fdce5651d0e91e561f3e08ecf684799f7f1ba0
Toren Caldwell <torencaldwell@gmail.com>
2017-09-29 11:27:47 -0500

    Branched a sandbox for non-destructive learning

--- app/src/main/java/edu/uark/csce/helpahog/MapsActivity.java
+++ app/src/main/java/edu/uark/csce/helpahog/MapsActivity.java
@@ -4,20 +4,26 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener{
-
+    private FusedLocationProviderClient mLocationClient;
     private GoogleMap mMap;
 
     @Override
@@ -34,12 +40,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    protected void createLocationRequest(int interval){
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(interval);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
         switch(requestCode){
             case 1: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(checkLocationPermission()) {
                     mMap.setMyLocationEnabled(true);
+
+                    mLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+                    mLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                Snackbar sb = Snackbar.make(findViewById(R.id.mapFrame), "Location Found", Snackbar.LENGTH_SHORT);
+                                sb.show();
+                            }
+                        }
+                    });
                 }else{
                     mMap.setMyLocationEnabled(false);
                 }
